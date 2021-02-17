4ffd04aaf0a1a412e5c7fd0d4d6d2a340fe42875
eurzua7020 <77946291+eurzua7020@users.noreply.github.com>
2021-02-17 15:54:22 -0500

    almost done

--- public/flappy/script.js
+++ public/flappy/script.js
@@ -1,182 +1,83 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('.grid')
-    const doodler = document.createElement('div')
-    let doodlerLeftSpace = 50
-    let startPoint = 150 
-    let doodlerBottomSpace = startPoint
+    const bird = document.querySelector('.bird')
+    const gameDisplay = document.querySelector('.game-container')
+    const ground = document.querySelector('.ground')
+
+    let birdLeft = 220
+    let birdBottom = 100
+    let gravity = 2
     let isGameOver = false
-    let platformCount = 5
-    let platforms = []
-    let upTimerId
-    let downTimerId
-    let isJumping = true
-    let isGoingLeft = false
-    let isGoingRight = false
-    let leftTimerId
-    let righTimerId 
-    let score = 0
-    
+    let gap = 430
 
-    function createDoodler(){
-        grid.appendChild(doodler)
-        doodler.classList.add('doodler')
-        doodlerLeftSpace = platforms[0].left
-        doodler.style.left = doodlerLeftSpace + 'px'
-        doodler.style.bottom = doodlerBottomSpace + 'px'
+    function startGame() {
+        birdBottom -= gravity
+        bird.style.bottom = birdBottom + 'px'
+        bird.style.left = birdLeft + 'px'
+        
     }
-
-    class Platform{
-        constructor(newPlatBottom){
-            this.bottom = newPlatBottom
-            this.left = Math.random() * 315
-            this.visual = document.createElement('div')
-
-            const visual = this.visual
-            visual.classList.add('platform')
-            visual.style.left = this.left + 'px'
-            visual.style.bottom = this.bottom + 'px'
-            grid.appendChild(visual) 
-        }
-    }
-
-    function createPlatforms(){
-        for(let i=0; i < platformCount; i++){
-            let platGap = 600 / platformCount
-            let newPlatBottom = 100 + i * platGap
-            let newPlatform = new Platform(newPlatBottom)
-            platforms.push(newPlatform)
-            console.log(platforms)
-        }
-    }
-
-    function movePlatforms() {
-        if (doodlerBottomSpace > 200) {
-            platforms.forEach(platform => {
-                platform.bottom -= 4
-                let visual = platform.visual
-                visual.style.bottom = platform.bottom + 'px'
-
-                if(platform.bottom <10) {
-                    let firstPlatform = platforms[0].visual
-                    firstPlatform.classList.remove('platform')
-                    platforms.shift()
-                    score++
-                    console.log(platforms)
-                    let newPlatform = new Platform(600)
-                    platforms.push(newPlatform)
-                }
-
-            })
-        }
-    }
-
-    function jump(){
-        clearInterval(downTimerId)
-        isJumping = true
-        upTimerId = setInterval(function(){
-            doodlerBottomSpace += 20
-            doodler.style.bottom = doodlerBottomSpace + 'px'
-            if (doodlerBottomSpace > startPoint + 200){
-                fall() 
-            }
-        },30)
-    }
-
-    function fall(){
-        clearInterval(upTimerId)
-        isJumping = false
-        downTimerId = setInterval(function(){
-            doodlerBottomSpace -= 5
-            doodler.style.bottom = doodlerBottomSpace + 'px'
-            if (doodlerBottomSpace <= 0){
-                gameOver()
-            }
-            platforms.forEach(platform => {
-                if(
-                    (doodlerBottomSpace >= platform.bottom) &&
-                    (doodlerBottomSpace <= platform.bottom) &&
-                    ((doodlerLeftSpace + 60) >= platform.left) &&
-                    (doodlerLeftSpace <= (platform.left +85)) &&
-                    !isJumping
-                ) {
-                    console.log('landed')
-                    startPoint = doodlerBottomSpace
-                    jump()
-                }
-            })
-
-        },30)
-    }
-
-    function gameOver(){
-        console.log('game over')
-        isGameOver = true
-        while (grid.firstChild){
-            grid.removeChild(grid.firstChild)
-        }
-        grid.innerHTML = score
-        clearInterval(upTimerId)
-        clearInterval(downTimerId)
-        clearInterval(leftTimerId)
-        clearInterval(righTimerId)
-    }
+    let gameTimerId = setInterval(startGame, 20)
+    /*clearInterval(timerId)*/
 
     function control(e){
-        if (e.key === "ArrowLeft"){
-            moveLeft()
-        }else if (e.key === "ArrowRight"){
-            moveRight()
-        }else if (e.key === "ArrowUp"){
-            moveStraight()
-        }
-    }
-
-    function moveLeft(){
-        if (isGoingRight) {
-            clearInterval(righTimerId)
-            isGoingRight = false
-        }
-        isGoingLeft = true
-        leftTimerId = setInterval(function() {
-            if (doodlerLeftSpace >= 0) {
-            doodlerLeftSpace -=5
-            doodler.style.left = doodlerLeftSpace + 'px'
-            } else moveRight()
-        },20)
-    }
-
-    function moveRight(){
-        if (isGoingLeft) {
-            clearInterval(leftTimerId)
-            isGoingLeft = false
-        }
-        isGoingRight = true
-        righTimerId = setInterval(function(){
-            if (doodlerLeftSpace <= 340){
-                doodlerLeftSpace += 5
-                doodler.style.left = doodlerLeftSpace + 'px'
-            } else moveLeft()
-        },20)
-    }
-
-    function moveStraight(){
-        isGoingRight = false
-        isGoingLeft = false
-        clearInterval(righTimerId)
-        clearInterval(leftTimerId)
-    }
-
-    function start () {
-        if (!isGameOver){
-            createPlatforms()
-            createDoodler()
-            setInterval(movePlatforms,30)
+        if (e.keyCode === 32) {
             jump()
-            document.addEventListener('keyup', control)
         }
     }
-    // attach to button
-   start()
-   
+
+
+    function jump() {
+        if (birdBottom < 500) birdBottom += 50
+        bird.style.bottom = birdBottom + 'px'
+        console.log(birdBottom)
+    }
+    document.addEventListener('keyup', control)
+
+    function generateObstacle(){
+        let obstacleLeft = 500
+        let randomHeight = Math.random() * 60
+        let obstacleBottom = randomHeight
+        const obstacle = document.createElement('div')
+        const topObstacle = document.createElement('div')
+        if (!isGameOver) {
+            obstacle.classList.add('obstacle')
+            topObstacle.classList.add('topObstacle')
+        }
+        gameDisplay.appendChild(obstacle)
+        gameDisplay.appendChild(topObstacle)
+        obstacle.style.left = obstacleLeft + 'px'
+        topObstacle.style.left = obstacleLeft + 'px'
+        obstacle.style.bottom = obstacleBottom + 'px'
+        topObstacle.style.bottom = obstacleBottom + gap + 'px'
+
+        function moveObstacle() {
+            obstacleLeft -=2
+            obstacle.style.left = obstacleLeft + 'px'
+            topObstacle.style.left = obstacleLeft + 'px'
+
+            if(obstacleLeft === -60){
+                clearInterval(timerId)
+                gameDisplay.removeChild(obstacle)
+                gameDisplay.removeChild(topObstacle)
+            }
+            if (
+                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
+                (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap -00) ||
+                birdBottom === 0) {
+                gameOver()
+                clearInterval(timerId)
+            }
+        }
+        let timerId = setInterval(moveObstacle, 20)
+        if (!isGameOver) setTimeout(generateObstacle, 3000)
+
+    }
+    generateObstacle()
+
+    function gameOver(){
+        clearInterval(gameTimerId)
+        isGameOver = true
+        document.removeEventListener('keyup', control)
+
+    }
 
 })
