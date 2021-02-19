c4c222b9cdb14865eedbd425ce2011e384935474
eurzua7020 <77946291+eurzua7020@users.noreply.github.com>
2021-02-19 16:54:16 -0500

    finished

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ app.route('/api')
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
     console.log('Now send something back to your client');
-    res.send('Hello World');
-    res.json({message: "Hello World"})
+    res.send('hello world');
+    res.json({message: "hello world"})
     /*res.json({data: dataToSendToFrontEnd});*/
   });
 
